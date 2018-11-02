92d2f88fdf58ad9ba2638f309a6a0665aefd2a33
Ocovert <42614487+Ocovert@users.noreply.github.com>
2018-11-02 14:12:46 -0500

    Solution To The Conversion Calculator Challenge

---
+++ Conversion Calculator/Conversion Calculator/Converter.swift
@@ -0,0 +1,15 @@
+//
+//  ColorsTableViewController.swift
+//  Colors Table
+//
+//  Created by Orrie on 10/28/18.
+//  Copyright © 2018 Tech innovator. All rights reserved.
+//
+
+import Foundation
+
+struct Converter{
+    var label:String
+    var input:String
+    var output:String
+}
